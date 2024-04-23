20f2ffdc53f4564acdea9d74a9e7cd2a22deb125
thanhdien02 <106503650+thanhdien02@users.noreply.github.com>
2024-04-23 11:34:23 +0700

    fix vercel

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,10 @@ import { store } from "./store/configureStore.ts";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <GoogleOAuthProvider clientId="577482635111-k3s954r3ldl1vkf7fnd60r4vrtnukb0a.apps.googleusercontent.com">
+    <GoogleOAuthProvider
+      //  clientId="577482635111-k3s954r3ldl1vkf7fnd60r4vrtnukb0a.apps.googleusercontent.com"
+      clientId="812530234129-9ri12aol4ntmq4sdlk2b4fqhl1rkr7uv.apps.googleusercontent.com"
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
